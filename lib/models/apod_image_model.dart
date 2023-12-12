f17ae8dc70bbaf7ee0ce42a965251f5db591be60
leandrucarvalho <leandrucs@gmail.com>
2023-12-12 10:03:00 -0300

    create model

---
+++ lib/models/apod_image_model.dart
@@ -0,0 +1,31 @@
+class ApodImageModel {
+  String? copyright;
+  String? date;
+  String? explanation;
+  String? hdurl;
+  String? mediaType;
+  String? serviceVersion;
+  String? title;
+  String? url;
+
+  ApodImageModel(
+      {this.copyright,
+      this.date,
+      this.explanation,
+      this.hdurl,
+      this.mediaType,
+      this.serviceVersion,
+      this.title,
+      this.url});
+
+  ApodImageModel.fromJson(Map<String, dynamic> json) {
+    copyright = json['copyright'];
+    date = json['date'];
+    explanation = json['explanation'];
+    hdurl = json['hdurl'];
+    mediaType = json['media_type'];
+    serviceVersion = json['service_version'];
+    title = json['title'];
+    url = json['url'];
+  }
+}
---
+++ lib/models/apod_pagination_model.dart
@@ -0,0 +1,11 @@
+class ApodPaginationModel {
+  final String? startDate;
+  final String? endDate;
+  final int? count;
+
+  ApodPaginationModel({
+    this.startDate,
+    this.endDate,
+    this.count,
+  });
+}
